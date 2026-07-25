1623f40310b516bf7480359346b5a176520a5ba8
mugheeskhan5 <khanmughees587@gmail.com>
2026-07-25 23:16:33 +0500

    Updated rule

--- scripts/pipeline.py
+++ scripts/pipeline.py
@@ -172,7 +172,7 @@ if __name__ == "__main__":
             "trigger_args": [
                 "/create",
                 "/tn",
-                "T1053_005_207",
+                "T1053_005_208",
                 "/sc",
                 "onstart",
                 "/ru",
